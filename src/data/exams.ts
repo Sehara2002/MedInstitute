396b0c8f8bc62a3cc66c2ad89b69824111f2b18f
Sehara2002 <shehara1010@gmail.com>
2026-07-14 22:24:18 +0530

    Removing Old Exams

--- src/data/exams.ts
+++ src/data/exams.ts
@@ -18,32 +18,32 @@ export interface Exam {
 }
 
 export const exams: Exam[] = [
-    {
-        id: "osce-mock-circuit",
-        title: "MRCGP INT. OSCE Mock Exam",
-        description: "Realistic Exam simulation with an experienced examiner. Ideal Marking Grid driven assessment & feedback for each candidate for all 14 cases. Also Unique exam tips will be given.",
-        date: "22nd MARCH 2026",
-        time: "9.00 AM ONWARDS",
-        loc:"Colombo, Sri Lanka",
-        fee: "LKR 16,000 per slot",
-        type: "Mock Exam",
-        offer: "LIMITED SLOTS AVAILABLE!",
-        image: ExPost1
-    },
+    // {
+    //     id: "osce-mock-circuit",
+    //     title: "MRCGP INT. OSCE Mock Exam",
+    //     description: "Realistic Exam simulation with an experienced examiner. Ideal Marking Grid driven assessment & feedback for each candidate for all 14 cases. Also Unique exam tips will be given.",
+    //     date: "22nd MARCH 2026",
+    //     time: "9.00 AM ONWARDS",
+    //     loc:"Colombo, Sri Lanka",
+    //     fee: "LKR 16,000 per slot",
+    //     type: "Mock Exam",
+    //     offer: "LIMITED SLOTS AVAILABLE!",
+    //     image: ExPost1
+    // },
 
-    {
-        id: "osce-mock-circuit-2",
-        title: "MRCGP INT. OSCE Mock Exam",
-        description: "Realistic Exam simulation with an experienced examiner. Ideal Marking Grid driven assessment & feedback for each candidate for all 14 cases. Also Unique exam tips will be given.",
-        date: "29th MARCH 2026",
-        time: "9.00 AM ONWARDS",
-        loc:"Colombo, Sri Lanka",
-        fee: "LKR 20,000 per slot",
-        type: "Mock Exam",
-        offer: "LIMITED SLOTS AVAILABLE!",
-        image: ExPost2
-    }
-    ,
+    // {
+    //     id: "osce-mock-circuit-2",
+    //     title: "MRCGP INT. OSCE Mock Exam",
+    //     description: "Realistic Exam simulation with an experienced examiner. Ideal Marking Grid driven assessment & feedback for each candidate for all 14 cases. Also Unique exam tips will be given.",
+    //     date: "29th MARCH 2026",
+    //     time: "9.00 AM ONWARDS",
+    //     loc:"Colombo, Sri Lanka",
+    //     fee: "LKR 20,000 per slot",
+    //     type: "Mock Exam",
+    //     offer: "LIMITED SLOTS AVAILABLE!",
+    //     image: ExPost2
+    // }
+    // ,
     {
         id: "osce-mock-circuit-3",
         title: "MRCGP INT. OSCE Mock Exam",
